b941980021a31a45bde4834eb30125f7eb60e603
jeppe-pape <jeppepape@live.dk>
2021-05-13 21:43:21 +0200

    Added phylo

--- extract_relations.py
+++ extract_relations.py
@@ -52,27 +52,31 @@ def collect():
 
 	df.to_csv("matrix.csv")
 
-#collect()
-df = pd.read_csv("matrix.csv", index_col=0)
-G = nx.from_pandas_adjacency(df)
 
-nx.write_weighted_edgelist(G, "edgelist.csv", delimiter=",")
-edge_df = pd.read_csv("edgelist.csv",names=["From","To","Weight"])
-print(edge_df)
-pos = nx.drawing.layout.spring_layout(G, k=0.05, iterations=50, dim=3)
+def write_position_data(suffix = "", G=None):
 
-first = pd.DataFrame(edge_df["From"].map(pos).tolist(), columns=["P(0)", "P(1)", "P(2)"])
-second = pd.DataFrame(edge_df["To"].map(pos).tolist(), columns=["P(0)", "P(1)", "P(2)"])
-print(first)
-first.to_csv("first_edge.csv")
-second.to_csv("second_edge.csv")
-out_df = pd.DataFrame.from_dict(pos, orient="index",columns=["P(0)", "P(1)", "P(2)"])
+	#Vertex Data
+	if G is None:
+		df = pd.read_csv("matrix.csv", index_col=0)
+		G = nx.from_pandas_adjacency(df)
+	pos = nx.drawing.layout.spring_layout(G, k=0.05, iterations=1, dim=3)
+	out_df = pd.DataFrame.from_dict(pos, orient="index",columns=["P(0)", "P(1)", "P(2)"])
+	out_df.to_csv("positions"+ suffix + ".csv")
+	print("Did vertices")
+	#Edge Data
+	nx.write_weighted_edgelist(G, "edgelist" + suffix + ".csv", delimiter=",")
+	edge_df = pd.read_csv("edgelist" + suffix + ".csv",names=["From","To","Weight"])
 
-#print(out_df)
-out_df.to_csv("positions.csv")
+	first = pd.DataFrame(edge_df["From"].map(pos).tolist(), columns=["P(0)", "P(1)", "P(2)"])
+	second = pd.DataFrame(edge_df["To"].map(pos).tolist(), columns=["P(0)", "P(1)", "P(2)"])
 
-def draw():
-	nx.draw_networkx(G, pos=nx.drawing.layout.spring_layout(G, k=0.05, iterations=50), node_size=20,
+	first.to_csv("first_edge" + suffix + ".csv")
+	second.to_csv("second_edge" + suffix + ".csv")
+
+ 
+
+def draw(G):
+	nx.draw_networkx(G, pos=nx.drawing.layout.spring_layout(G, iterations=50), node_size=20,
 		node_color=(0,0,0),
 		alpha = 0.1,
 		edge_cmap=plt.viridis,
@@ -82,3 +86,13 @@ def draw():
 		font_size = 10)
 	plt.axis("off")
 	plt.show()
+
+def get_taxonomy():
+	tax = pd.read_csv("redlist_data/taxonomy.csv")
+	tax = tax.iloc[:,2:9]
+	G = nx.Graph()
+	for i, row in enumerate(tax.iterrows()):
+		for index, vertex in enumerate(row[:-1]):
+			G.add_edge(vertex, index)
+	write_position_data(G=G, suffix = "_species")
+get_taxonomy()
